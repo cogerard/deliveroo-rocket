cef15dec5861ad544dee49b82d0fe435ebd96708
Corentin Gerard <corentin_gerard@MacBook-Pro-de-Corentin.local>
2024-04-29 15:07:07 +0200

    first commit

---
+++ src/components/Category.jsx
@@ -0,0 +1,18 @@
+import Meal from "./Meal";
+
+const Category = ({ category, handleAddToCart }) => {
+  return (
+    <div className="category">
+      <h2>{category.name}</h2>
+      <div className="meals-container">
+        {category.meals.map((meal) => {
+          return (
+            <Meal key={meal.id} meal={meal} handleAddToCart={handleAddToCart} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/components/Meal.jsx
@@ -0,0 +1,26 @@
+const Meal = ({ meal, handleAddToCart }) => {
+  return (
+    <div
+      className="meal"
+      onClick={() => {
+        handleAddToCart(meal);
+      }}
+    >
+      <div>
+        <p>{meal.title}</p>
+        {meal.description && (
+          <div className="description-container">
+            <p>{meal.description}</p>
+          </div>
+        )}
+        <div className="price-popular-container">
+          <p>{meal.price} €</p>
+          {meal.popular && <p style={{ color: "orange" }}>popular</p>}
+        </div>
+      </div>
+      {meal.picture && <img src={meal.picture} alt="" />}
+    </div>
+  );
+};
+
+export default Meal;
