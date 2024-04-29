f78c05928fda8a46a09c3bf13ba37a7d6f1b6e2a
Corentin Gerard <corentin_gerard@MacBook-Pro-de-Corentin.local>
2024-04-29 16:39:10 +0200

    prettier extension

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ function App() {
           </section>
         </div>
         <button
-          className="theme-button"
+          className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
           onClick={() => {
             myInput.current.focus();
           }}
